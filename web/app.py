9202f830005759ac9c4204a6eec0c1b17730ce4f
akocukcu <akocukcu@gmail.com>
2020-04-26 01:46:44 +0300

    single equal sign bug fixed

--- web/app.py
+++ web/app.py
@@ -141,5 +141,5 @@ api.add_resource(Register, '/register')
 api.add_resource(Detect, '/detect')
 api.add_resource(Refill, '/refill')
 
-if __name__ = "__main__":
+if __name__ == "__main__":
 	app.run(host='0.0.0.0')
